05f9bcaaec2c2e3ed43b44550ea7a13b470edab1
LorenzoPorrasDuque <lorenzo.porras.duque@gmail.com>
2023-05-19 17:25:34 -0500

    Graficas y medio nav

--- Components/navigationMenu.js
+++ Components/navigationMenu.js
@@ -1,25 +1,57 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Pressable } from "react-native";
 import { Entypo, Feather, AntDesign, MaterialIcons } from "@expo/vector-icons";
-import { NavigationContext } from "@react-navigation/native";
-import React from "react";
-import { useContext } from "react";
+import {
+  NavigationContext,
+  useNavigationState,
+} from "@react-navigation/native";
+import React, { useEffect } from "react";
+import { useContext, useState } from "react";
 
 export default function NavigationMenu() {
   const navigation = useContext(NavigationContext);
+  const [path, setPath] = useState("");
+  const state = useNavigationState((state) => state);
+  const routeName = state.routeNames[state?.index];
+
+  useEffect(() => {
+    for (const i in state.routes) {
+      if (parseInt(i) === state?.index) {
+        console.log(state.routes[i].name);
+        setPath(state.routes[i].name);
+      }
+    }
+  }, [routeName]);
+
   return (
     <View style={styles.container}>
       <Pressable onPress={() => navigation.navigate("home")}>
-        <Entypo name="home" size={30} color="black" />
+        <Entypo
+          name="home"
+          size={30}
+          color={path === "home" ? "blue" : "black"}
+        />
       </Pressable>
       <Pressable onPress={() => navigation.navigate("profile")}>
-        <Feather name="pie-chart" size={30} color="black" />
+        <Feather
+          name="pie-chart"
+          size={30}
+          color={path === "profile" ? "blue" : "black"}
+        />
       </Pressable>
       <Pressable onPress={() => navigation.navigate("chart_screen")}>
-        <AntDesign name="antdesign" size={30} color="black" />
+        <AntDesign
+          name="antdesign"
+          size={30}
+          color={path === "chart_screen" ? "blue" : "black"}
+        />
       </Pressable>
       <Pressable onPress={() => navigation.navigate("payment")}>
-        <MaterialIcons name="payment" size={30} color="black" />
+        <MaterialIcons
+          name="payment"
+          size={30}
+          color={path === "payment" ? "blue" : "black"}
+        />
       </Pressable>
     </View>
   );
--- Screens/ChartScreen.js
+++ Screens/ChartScreen.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Pressable } from "react-native";
 import NavigationMenu from "../Components/navigationMenu";
+import { LineChart } from "react-native-chart-kit";
 
 export default function ChartScreen({ navigation }) {
   return (
@@ -8,6 +9,49 @@ export default function ChartScreen({ navigation }) {
       <NavigationMenu></NavigationMenu>
       <Text>graficas y mierdas</Text>
       <StatusBar style="auto" />
+      <LineChart
+        data={{
+          labels: ["January", "February", "March", "April", "May", "June"],
+          datasets: [
+            {
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+              ],
+            },
+          ],
+        }}
+        width={400} // from react-native
+        height={220}
+        yAxisLabel="$"
+        yAxisSuffix="k"
+        yAxisInterval={1} // optional, defaults to 1
+        chartConfig={{
+          backgroundColor: "#e26a00",
+          backgroundGradientFrom: "#fb8c00",
+          backgroundGradientTo: "#ffa726",
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726",
+          },
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+      />
     </View>
   );
 }
@@ -20,3 +64,14 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+const chartConfig = {
+  backgroundGradientFrom: "#1E2923",
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: "#08130D",
+  backgroundGradientToOpacity: 0.5,
+  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+  strokeWidth: 2, // optional, default 3
+  barPercentage: 0.5,
+  useShadowColorFromDataset: false, // optional
+};
